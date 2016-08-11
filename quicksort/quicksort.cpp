5a280cf55098c16f3a26a341317fc8b12a50d15f
ffreylaw <ffreylaw@gmail.com>
2016-08-11 20:10:12 +1000

    quicksort

---
+++ quicksort/quicksort.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define SIZE 10
+
+void quickSort(vector<int> &arr, int left, int right);
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int> arr(SIZE);
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = rand() % 20;
+    }
+    quickSort(arr, 0, SIZE - 1);
+    for (int i = 0; i < SIZE; i++) {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
+
+void quickSort(vector<int> &arr, int left, int right) {
+      int i = left, j = right;
+      int tmp;
+      int pivot = arr[(left + right) / 2];
+
+      /* partition */
+      while (i <= j) {
+            while (arr[i] < pivot)
+                  i++;
+            while (arr[j] > pivot)
+                  j--;
+            if (i <= j) {
+                  tmp = arr[i];
+                  arr[i] = arr[j];
+                  arr[j] = tmp;
+                  i++;
+                  j--;
+            }
+      };
+
+      /* recursion */
+      if (left < j)
+            quickSort(arr, left, j);
+      if (i < right)
+            quickSort(arr, i, right);
+}
